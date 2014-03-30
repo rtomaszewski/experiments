f8bd85388dcb4b9121a7ac2fe113e1e8d70e9dc0
Radoslaw Tomaszewski <radoslaw.tomaszewski@gmail.com>
2014-03-30 20:13:20 +0100

    - add variables

--- type-command.c
+++ type-command.c
@@ -7,26 +7,53 @@
 #include <termios.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-main()
-{
-    char buf[] = "date";
-    int i;
+const char *TYPE_CMD_ENABLED = "TYPE_CMD_ENABLED";
+const char *TYPE_CMD_TYPE = "TYPE_CMD_TYPE";
+
+const char *PROGRAM_NAME = "type-command";
+
+
+int main(void) {
     struct termios oldtio,newtio;
     int fd=0;
+    int i;
 
-    tcgetattr(fd,&oldtio); /* save current serial port settings */
-    bzero(&newtio, sizeof(newtio)); /* clear struct for new port settings */
+    char *is_enabled;
+    char *command;
+
+    // read the bash variables
+    is_enabled = getenv(TYPE_CMD_ENABLED);
+    command = getenv(TYPE_CMD_TYPE);
+
+    // is the var defined at all
+    if ( ! is_enabled ) {
+        printf("%s: the variable %s is not set, set it to 'no' to surpress this message\n", PROGRAM_NAME, TYPE_CMD_ENABLED);
+        return 0;
+
+    // has the variable value different than 'yes'
+    } else if ( strcmp(is_enabled, "yes") ) {
+        return 0;
+    }
+
+    if ( ! command ) {
+        return 0;
+    }
+
+    tcgetattr(fd,&oldtio);                      // save current terminal  settings
+    bzero(&newtio, sizeof(newtio));             // clear struct for new port settings
 
     memcpy(&newtio, &oldtio, sizeof(oldtio) );
-    newtio.c_lflag &= ~ECHO;
+    newtio.c_lflag &= ~ECHO;                    // do not echo the input chars
 
-    tcflush(fd, TCIFLUSH);
+    tcflush(fd, TCIFLUSH);                      // set the new terminal settings
     tcsetattr(fd,TCSANOW,&newtio);
 
-    for (i = 0; i < sizeof buf - 1; i++)
-      ioctl(fd, TIOCSTI, &buf[i]);
+    for (i = 0; i < strlen(command); i++)
+      ioctl(fd, TIOCSTI, &command[i]);
 
     tcsetattr(fd,TCSANOW,&oldtio);
     return 0;
 }
+
