051a4416c0d346975100591104b5eec4e91c4b7d
Radoslaw Tomaszewski <radoslaw.tomaszewski@gmail.com>
2014-10-19 17:04:16 +0100

    Update nova_example.py

--- nova_example.py
+++ nova_example.py
@@ -1,8 +1,8 @@
 import httplib2
 httplib2.debuglevel = 1
 
-OS_USERNAME="ukcloudnova"
-OS_PASSWORD="f8d32c75eafaa46d05c4de2919489e67"
+OS_USERNAME="ukcloud-username"
+OS_PASSWORD="pass"
 OS_AUTH_URL="https://identity.api.rackspacecloud.com/v2.0/"
 
 from novaclient.v1_1 import client
